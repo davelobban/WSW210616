6da4b8f3eb541e5dd97fbb0329b672f0885767ab
Dave Lobban <dave.lobban@atmosi.com>
2021-06-16 18:33:07 +0100

    Add SnackLines

--- WSW210616/PetrolStation.cs
+++ WSW210616/PetrolStation.cs
@@ -11,14 +11,19 @@ namespace WSW210616
 
     public record Transaction
     {
-        private List<FuelLine> _lines = new ();
-        public void Add(FuelLine value)
+        private List<TransactionLine> _lines = new ();
+        public void Add(TransactionLine value)
         {
             _lines.Add(value);
         }
-
-        public decimal GetTotal(DateTime dateTime)=>_lines.Sum(l => l.Litres) * 1.25m * (dateTime.Hour >= 23 || dateTime.Hour < 7 ? 0.95m : 1m);
+        public decimal GetTotal(DateTime dateTime)=>_lines.Where(l=>l is FuelLine).Select(l=>l as FuelLine).Sum(l => l.Litres) * 1.25m * 1.05m* (dateTime.Hour >= 23 || dateTime.Hour < 7 ? 0.95m : 1m)
+        + _lines.Where(l => l is SnackLine).Select(l => l as SnackLine).Sum(l => l.ItemCount* l.ItemPrice) * (dateTime.Hour >= 23 || dateTime.Hour < 7 ? 0.95m : 1m);
     }
-    public record FuelLine(decimal Litres);
+    public record FuelLine(decimal Litres):TransactionLine;
 
+    public record SnackLine(decimal ItemPrice, int ItemCount) : TransactionLine;
+
+    public record TransactionLine
+    {
+    }
 }
--- WSW210616Test/UnitTest1.cs
+++ WSW210616Test/UnitTest1.cs
@@ -13,6 +13,17 @@ namespace WSW210616Test
         //When it is between 2300 and 0700 local time
         //Then a 5% off peak discount is applied to the fuel.
 
+        //BDD 
+        //As a petrol station transaction
+        //When it is a Fuel Line is added to the transaction 
+        //Then the fuel attracts a VAT of 5%.
+
+        //BDD 
+        //As a petrol station transaction
+        //When it is a Snack Line is added to the transaction 
+        //Then the fuel attracts a VAT of 20%.
+
+
         [Test]
         public void Test2300()
         {
@@ -22,7 +33,7 @@ namespace WSW210616Test
             t.Add(new FuelLine(50.5m));
             //Act: 
             var actual = t.GetTotal(new DateTime(2021, 6, 16, 23, 00, 00));
-            var expectedFuelCost = 50.5 * 1.25 * .95;
+            var expectedFuelCost = 50.5 * 1.25 * .95 *1.05;
             //Assert
             Assert.AreEqual(expectedFuelCost, actual);
         }
@@ -35,7 +46,21 @@ namespace WSW210616Test
             t.Add(new FuelLine(50.5m));
             //Act: 
             var actual = t.GetTotal(new DateTime(2021, 6, 16, 6, 59, 59));
-            var expectedFuelCost = 50.5 * 1.25 * .95;
+            var expectedFuelCost = 50.5 * 1.25 * .95 * 1.05;
+            //Assert
+            Assert.AreEqual(expectedFuelCost, actual);
+        }
+        [Test]
+        public void Test065959PlusTwoSnacks()
+        {
+            //Arrange the petrol station and the transaction
+            var sut = new PetrolStation();
+            var t = sut.NewTransaction();
+            t.Add(new SnackLine(10.1m, 5)); 
+            t.Add(new FuelLine(50.5m));
+            //Act: 
+            var actual = t.GetTotal(new DateTime(2021, 6, 16, 6, 59, 59));
+            var expectedFuelCost = 110.9421875m;
             //Assert
             Assert.AreEqual(expectedFuelCost, actual);
         }
@@ -48,11 +73,24 @@ namespace WSW210616Test
             t.Add(new FuelLine(50.5m));
             //Act: 
             var actual = t.GetTotal(new DateTime(2021, 6, 17, 6, 59, 59));
-            var expectedFuelCost = 50.5 * 1.25 * .95;
+            var expectedFuelCost = 50.5 * 1.25 * .95 * 1.05;
             //Assert
             Assert.AreEqual(expectedFuelCost, actual);
         }
-
+        [Test]
+        public void Test065959TmrwPlusTwoSnacks()
+        {
+            //Arrange the petrol station and the transaction
+            var sut = new PetrolStation();
+            var t = sut.NewTransaction();
+            t.Add(new FuelLine(50.5m));
+            t.Add(new SnackLine(10.1m, 5));
+            //Act: 
+            var actual = t.GetTotal(new DateTime(2021, 6, 17, 6, 59, 59));
+            var expectedFuelCost = 110.9421875m;
+            //Assert
+            Assert.AreEqual(expectedFuelCost, actual);
+        }
         [Test]
         public void Test225959()
         {
@@ -62,7 +100,7 @@ namespace WSW210616Test
             t.Add(new FuelLine(50.5m));
             //Act: 
             var actual = t.GetTotal(new DateTime(2021, 6, 16, 22, 59, 59));
-            var expectedFuelCost = 50.5 * 1.25;
+            var expectedFuelCost = 50.5 * 1.25 * 1.05;
             //Assert
             Assert.AreEqual(expectedFuelCost, actual);
         }
@@ -75,18 +113,12 @@ namespace WSW210616Test
             t.Add(new FuelLine(50.5m));
             //Act: 
             var actual = t.GetTotal(new DateTime(2021, 6, 16, 07, 0, 0));
-            var expectedFuelCost = 50.5 * 1.25;
+            var expectedFuelCost = 50.5 * 1.25 * 1.05;
             //Assert
             Assert.AreEqual(expectedFuelCost, actual);
         }
-
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+        
     }
 
-    
+
 }
